ce998c7a88713525e8b4586770621b70e5a84401
Abdoelabassi <abderrezaq@outlook.com>
2023-03-22 11:11:39 +0000

    e+ gun in random position

--- day2/ExDay2/src/YourPrimaryGeneratorAction.cc
+++ day2/ExDay2/src/YourPrimaryGeneratorAction.cc
@@ -5,7 +5,8 @@
 #include "G4Event.hh"
 #include "Randomize.hh"
 #include "G4SystemOfUnits.hh"
-
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
 
 YourPrimaryGeneratorAction::YourPrimaryGeneratorAction()
   : G4VUserPrimaryGeneratorAction()
@@ -25,8 +26,17 @@ YourPrimaryGeneratorAction::~YourPrimaryGeneratorAction()
 void YourPrimaryGeneratorAction::GeneratePrimaries(G4Event* evt)
 {
    // here it is possible to place extra code
-
-   // 
+   //Define random positions
+   G4double x = 1.1*cm;
+   G4double y = (-1.25 + 2.5*G4UniformRand())*cm;
+   G4double z = (-1.25 + 2.5*G4UniformRand())*cm;
+   //G4ParticleDefinition* particle;
+   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+   G4ParticleDefinition* particle = particleTable->FindParticle("e+");
+   fParticleGun->SetParticleDefinition(particle);
+   fParticleGun->SetParticleEnergy(100*MeV);
+   fParticleGun->SetParticlePosition(G4ThreeVector(x,y,z));
+   //
    fParticleGun->GeneratePrimaryVertex(evt);
 }
 
