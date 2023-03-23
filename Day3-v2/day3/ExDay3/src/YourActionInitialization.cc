dd09244c259d11ae8d32e11d370690c27db3574e
Abdoelabassi <abderrezaq@outlook.com>
2023-03-23 11:42:51 +0000

    init constructor

--- Day3-v2/day3/ExDay3/src/YourActionInitialization.cc
+++ Day3-v2/day3/ExDay3/src/YourActionInitialization.cc
@@ -3,7 +3,7 @@
 
 #include "YourPrimaryGeneratorAction.hh"
 
-// #include "YourDetectorConstruction.hh"
+#include "YourDetectorConstruction.hh"
 
 #include "YourEventAction.hh"
 #include "YourSteppingAction.hh"
@@ -11,25 +11,25 @@
 YourActionInitialization::YourActionInitialization()
   : G4VUserActionInitialization()
 {
-   // CHANGE this 
+   // CHANGE this
    // We need to get/keep a pointer to 'YourDetectorConstruction'
 }
-    
+
 
 YourActionInitialization::~YourActionInitialization()
 {
 }
 
 
-// NOT CALLED IN SEQUENTIAL MODE i.e. ONLY FOR MT: 
+// NOT CALLED IN SEQUENTIAL MODE i.e. ONLY FOR MT:
 // - ONLY FOR creating RunAction for the Master thread
 void YourActionInitialization::BuildForMaster() const
 {
 }
 
 
-// Create all User Actions here: 
-// - it will be invoked immediately by G4RunManager 
+// Create all User Actions here:
+// - it will be invoked immediately by G4RunManager
 //   when the ActionInitialization object is registered in the main
 void YourActionInitialization::Build() const
 {
@@ -37,14 +37,17 @@ void YourActionInitialization::Build() const
   YourPrimaryGeneratorAction* primaryAction = new YourPrimaryGeneratorAction();
   SetUserAction(primaryAction);
 
-
-  YourSteppingAction* stepAction = new YourSteppingAction();  // CHANGE this 
+  const G4VPhysicalVolume* targetVolume = fDetectorCon->GetTargetPhysicalVolume();
+  if( targetVolume == nullptr ){
+      G4cerr << "WARNING>  target Volume is not (yet) defined !! " << G4endl;
+  }
+  YourSteppingAction* stepAction = new YourSteppingAction(targetVolume);  // CHANGE this
   //   The SteppingAction needs the target volume from the DetectorConstruction !!
 
   // Set UserSteppingAction
   SetUserAction( stepAction );
-  
+
     // Set UserEventAction
   YourEventAction* eventAction = new YourEventAction(stepAction);
   SetUserAction(eventAction);
-}  
+}
