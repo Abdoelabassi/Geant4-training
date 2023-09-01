2807eeaef49f7aca491b5b3f0c840475a4ddb9c7
Abdoelabassi <abderrezaq@outlook.com>
2023-09-01 10:47:06 +0100

    first geant4 simulation

--- practice/cherenkova.cc
+++ practice/cherenkova.cc
@@ -2,18 +2,21 @@
 #include "G4RunManager.hh"
 #include "G4UIExecutive.hh"
 #include "G4VisExecutive.hh"
+#include "G4UImanager.hh"
 
 
 
 int main(int argc, char** argv){
 
     G4RunManager* runManager = new G4RunManager();
-    G4UIEexecutive* ui = new G4UIExecutive(argc, argv);
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
 
     G4VisManager* visManager = new G4VisExecutive();
     visManager->Initialize();
 
     G4UImanager *uiManager = G4UImanager::GetUIpointer();
+
+    ui->SessionStart();
 
 
 
